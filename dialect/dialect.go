f00ba96fc317aba0ba25e5dada895ad915fc489a
likeran <windflyme5@gmail.com>
2020-12-24 18:15:43 +0800

    dialect映射

--- dialect/dialect.go
+++ dialect/dialect.go
@@ -1,1 +1,21 @@
 package dialect
+// 数据库与Golang的类型映射
+
+import "reflect"
+
+type Dialect interface {
+	DataTypeOf(typ reflect.Value) string
+	// 用于将 Go 语言的类型转换为该数据库的数据类型。
+	TableExistSQL(tableName string) (string, []interface{})
+}
+
+var dialectsMap = map[string]Dialect{}
+
+func RegisterDialect(name string, dialect Dialect) {
+	dialectsMap[name] = dialect
+}
+
+func GetDialect(name string) (dialect Dialect,ok bool) {
+	dialect, ok = dialectsMap[name]
+	return dialect, ok
+}
--- easyorm.go
+++ easyorm.go
@@ -1,5 +1,38 @@
 package EasyOrm
 
-type Engine struct {
+import "database/sql"
 
+import "./log"
+import "./session"
+
+type Engine struct {
+	db *sql.DB
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	// 加载驱动
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	e = &Engine{db}
+	log.Info("Connect database success")
+	return
+}
+
+func (engine *Engine) Close() {
+	err := engine.db.Close()
+	if err != nil {
+		log.Error("failed to close database")
+	}
+	log.Info("Close database success")
+}
+
+func (engine *Engine) NewSession() *session.Session {
+	return session.New(engine.db)
 }
